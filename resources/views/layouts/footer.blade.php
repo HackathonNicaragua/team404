8851ab9f97dad579c38f703abe24e1a7f1ad1c02
Edwinj10 <edwinjosealtamirano@gmail.com>
2017-11-25 13:05:57 -0600

    Pagina Inicio

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,99 @@
+<!--Footer-->
+<footer class="page-footer stylish-color-dark">
+
+    <!--Footer Links-->
+    <div class="container">
+
+        <!-- Footer links -->
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-12">
+                <img src="img/logo/Logo blanco.png" alt="">
+            </div>
+            <!--/.First column-->
+            <div class="col-md-10 col-md-offset-1">
+                <div class="col-md-7">
+                    <div class="col-md-6">
+                        
+                        <p><a href="#!">Quiénes somos</a></p>
+                        <p><a href="#!">Preguntas Frecuentes</a></p>
+                        <p><a href="#!">Contácto</a></p>
+                        <p><a href="#!">Soporte Técnico</a></p>
+                    </div>
+                    <!--/.Second column-->
+
+                    <!--Third column-->
+                    <div class="col-md-6">
+                        <p><a href="#!">Certificaciones</a></p>
+                        <p><a href="#!">Seguridad</a></p>
+                        <p><a href="#!">Nuestros socios</a></p>
+                        <p><a href="#!">Guia para padres</a></p>
+                    </div>
+                
+                </div>
+                <div class="col-md-5">
+                    <br>
+                    <p class="color-white">
+                        Obtenga información acerca de Educarte.ni
+                    </p>
+                    
+                    <form class="col-md-12">
+                        <div class="row">
+                            <div class="input-field col-md-10">
+                                <input id="email" type="email" class="validate">
+                                <label for="email" data-error="wrong" data-success="right">Email</label>
+                            </div>
+                            <div class="col-md-2">
+                                <button class="btn btn-env btn-rounded waves-effect waves-light"> Suscribirse</button>
+                            </div>
+                        </div>
+
+                        <p>
+                            <input type="checkbox" id="test6" checked="checked" />
+                            <label for="test6" class="color-white">Estoy deacuerdo con las condiciones de uso</label>
+                        </p>
+                    </form>
+                </div>
+            </div>
+
+           
+        </div>
+        <!-- Footer links -->
+
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-md-8 col-lg-9">
+                <div class="col-md-6">
+                    <p class="color-white">© 2017 Copyright: <a href=""><strong> Educarte.ni</strong></a></p>
+                </div>
+                <div class="col-md-6">
+                    <a href="" class="enlacesfot">Condiciones de uso</a> 
+                    <a href="" class="enlacesfot">Politicas de privacidad</a>
+                </div>
+                <!--/.Copyright-->
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 col-lg-3">
+
+                <!--Social buttons-->
+                <div class="text-center text-md-left">
+                    <ul>
+                        <a class="btn-floating btn-large"><i class="fa fa-facebook"> </i></a>
+                     
+                        <a class="btn-floating btn-large"><i class="fa fa-google-plus"> </i></a>
+                    </ul>
+                </div>
+                <!--/.Social buttons-->
+
+            </div>
+            <!--Grid column-->
+
+        </div>
+
+    </div>
+
+</footer>
+<!--/.Footer-->
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+     <title>
+        @yield('title')-Educarte
+    </title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="font/css/font-awesome.css">
+    <link rel="stylesheet" href="mdb/css/mdb.css">
+    <link rel="stylesheet" href="css/style.css">
+
     
 </head>
 <body>
+    @include("layouts.nav")
     <div class="container">
-        @include("layouts.nav")
-
-        @yield('contenido')
-
-    
+        <div class="col-md-12">
+            @yield('contenido')
+        </div>
     </div>
+    @include('layouts.footer')
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="mdb/js/mdb.js"></script>
+
 </body>
 </html>
