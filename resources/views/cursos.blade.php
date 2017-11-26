79b272b6c2483e7876d4f1c7f38dcf2aa364328f
Fregdy Torres <fregdytorres@gmail.com>
2017-11-25 22:50:04 -0600

    se finalizo el diseño del detalle de cursos por las principales categorias. se agrego el modal para un mayor detalle de cada curso.

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -43,28 +43,54 @@
                         </div>
                     </div>
                     <div class="row">
-                        @foreach ($area_row->cursos as $cursos)
-                        <div class="input-field col-md-4">
-                            <div class="card">
-                                <div class="card-image">
-                                    <div class="view overlay hm-white-slight z-depth-1">
-                                        <img src="https://mdbootstrap.com/images/reg/reg%20(2).jpg" class="img-responsive" alt="">
-                                        <a href="#">
-                                            <div class="mask waves-effect"></div>
-                                        </a>
+                        @foreach ($area_row->cursos as $curso)
+                        <div class="card hoverable col-md-4" data-toggle="modal" data-target="#modal-{{$curso->id}}">
+                            <div class="card-image">
+                                <div class="view overlay hm-white-slight z-depth-1">
+                                    <img src="https://mdbootstrap.com/images/reg/reg%20(2).jpg" class="img-responsive" alt="">
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <h5>{{$curso->titulo}}</h5>
+                            </div>
+                            <!--Buttons-->
+                            <div class="card-btn text-center row">
+                                <div class="rating col-md-4 color-blue-dark">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o"></i>
+                                </div>
+                                <ul class="list-inline item-details col-md-4 color-blue-dark">
+                                    <li>2h<i class="fa fa-clock-o"></i></li>
+                                    <li>312<i class="fa fa-user-o"></i></li>
+                                </ul>
+                                <div class="col-md-4"> 
+                                    <label style="font-size: large;" class="color-green-vibrant">Gratis</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal fade" id="modal-{{$curso->id}}" role="dialog">
+                            <div class="modal-dialog">
+                                <!-- Modal content-->
+                                <div class="modal-content">
+
+                                    <div class="modal-header">
+                                        <div class="view overlay hm-white-slight z-depth-1">
+                                            <img src="https://mdbootstrap.com/images/reg/reg%20(2).jpg" class="img-responsive" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h4 class="modal-title">{{$curso->titulo}}</h4>
+                                        <p>{{$curso->descripcion}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                     </div>
                                 </div>
-                                <div class="card-content">
-                                    <h5>{{$cursos->titulo}}</h5>
-                                </div>
-                                <!--Buttons-->
-                                <div class="card-btn text-center">
-                                    <a href="#" class="btn btn-primary btn-md waves-effect waves-light">Read more</a>
-                                    <a href="#" class="btn btn-default btn-md waves-effect waves-light">Buy now</a>
-                                </div>
-                                <!--/.Buttons-->
+
                             </div>
-                            
                         </div>
                         @endforeach
                     </div>
